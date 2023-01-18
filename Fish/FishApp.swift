7279c4a223285ffde061a81ef9883e227eecaeb5
Aaron Zheng <s029864@students.lmsd.org>
2023-01-18 09:04:30 -0500

    Initial Commit

---
+++ Fish/FishApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishApp.swift
+//  Fish
+//
+//  Created by Aaron Zheng (student LM) on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
